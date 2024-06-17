bcd23f3b59b62e59808ce3d55c39d8565d426de0
kysclient <kysclient@gmail.com>
2024-06-17 17:25:03 +0900

    refactor: optimize webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,66 @@
 const path = require("path");
+const { DefinePlugin } = require("webpack");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "bundle.js",
-    publicPath: "/",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: "esbuild-loader",
-        options: {
-          loader: "jsx",
-          target: "es2015",
-        },
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.(sass|css|scss)$/,
-        use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: "@svgr/webpack",
-            options: {
-              svgo: false,
-            },
-          },
-          "file-loader",
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".jsx"],
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "public"),
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
+
+  return {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "bundle.js",
+      publicPath: "/",
     },
-    compress: true,
-    port: "auto",
-    historyApiFallback: true,
-  },
-  mode: "development",
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          loader: "esbuild-loader",
+          options: {
+            loader: "jsx",
+            target: "es2015",
+          },
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(sass|css|scss)$/,
+          use: ["style-loader", "css-loader", "sass-loader", "postcss-loader"],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: "@svgr/webpack",
+              options: {
+                svgo: false,
+              },
+            },
+            "file-loader",
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".js", ".jsx"],
+    },
+    devServer: {
+      static: {
+        directory: path.join(__dirname, "public"),
+      },
+      compress: true,
+      port: "auto",
+      historyApiFallback: true,
+    },
+    mode: isProduction ? "production" : "development",
+    optimization: {
+      minimize: isProduction,
+    },
+    plugins: [
+      new DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(
+          isProduction ? "production" : "development"
+        ),
+      }),
+    ],
+  };
 };
